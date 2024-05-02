1538ace4efb105daeee00b032b97ef42ca74abe5
Blessing U <blessing986@users.noreply.github.com>
2024-05-02 17:33:15 +0100

    chore: added an event handler using the on method

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import { User } from "./models/User";
 
 const user = new User({ name: "myname", age: 23 });
 
-user.set({ name: "newname", age: 999 });
+// user.set({ name: "newname", age: 999 });
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+// console.log(user.get("name"));
+// console.log(user.get("age"));
+
+user.on("change", () => {});
+user.on("change", () => {});
+user.on("hgfy", () => {});
+
+console.log(user);
