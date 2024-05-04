5ae41d73a83fd1c76eaeea99c672db872d2dc045
Blessing U <blessing986@users.noreply.github.com>
2024-05-04 17:03:50 +0100

    chore: parsing User JSON

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,80 @@
-//////////////////////////////////////////////////
+// //////////////////////////////////////////////////
+
+// // import { User } from "./models/User";
+
+// // const user = new User({ name: "myname", age: 23 });
+
+// // // user.set({ name: "newname", age: 999 });
+
+// // // console.log(user.get("name"));
+// // // console.log(user.get("age"));
+
+// // user.on("change", () => {
+// //   console.log("Change #1");
+// // });
+// // user.on("change", () => {
+// //   console.log("Change #2");
+// // });
+// // user.on("save", () => {
+// //   console.log("Save was triggered");
+// // });
+
+// // user.trigger("save")
+
+// ////////////////////////////////////////////////
+
+// // import axios from "axios";
+
+// // // axios.post("http://localhost:3000/users", {
+// // //   name: "myname",
+// // //   age: 20,
+// // // });
+
+// // axios.get("http://localhost:3000/users/3be3");
+
+// //////////////////////////////////////////////////
 
 // import { User } from "./models/User";
 
-// const user = new User({ name: "myname", age: 23 });
+// // const user = new User({ id: "3be3" });
+// // const user = new User({ name: "new record", age: 0 });
+// const user = User.buildUser({ id: "650a" });
 
-// // user.set({ name: "newname", age: 999 });
+// //////////////////////////////////////
+// // user.fetch();
 
-// // console.log(user.get("name"));
-// // console.log(user.get("age"));
+// // setTimeout(() => {
+// //   console.log(user);
+// // }, 4000);
 
-// user.on("change", () => {
-//   console.log("Change #1");
-// });
-// user.on("change", () => {
-//   console.log("Change #2");
-// });
-// user.on("save", () => {
-//   console.log("Save was triggered");
-// });
+// //////////////////////////////////////////
+// //user.set({ name: "NEW NAME", age: 11 });
+// // user.save();
 
-// user.trigger("save")
-
-////////////////////////////////////////////////
-
-// import axios from "axios";
-
-// // axios.post("http://localhost:3000/users", {
-// //   name: "myname",
-// //   age: 20,
+// /////////////////////////////////////////
+// // user.events.on("change", () => {
+// //   console.log("change!");
 // // });
 
-// axios.get("http://localhost:3000/users/3be3");
+// // user.events.trigger("change");
 
-//////////////////////////////////////////////////
+// /////////////////////////////////////////
+// // console.log(user.get("name"));
 
-import { User } from "./models/User";
-
-// const user = new User({ id: "3be3" });
-// const user = new User({ name: "new record", age: 0 });
-const user = User.buildUser({ id: "650a" });
-
-//////////////////////////////////////
-// user.fetch();
-
-// setTimeout(() => {
+// user.on("change", () => {
 //   console.log(user);
-// }, 4000);
-
-//////////////////////////////////////////
-//user.set({ name: "NEW NAME", age: 11 });
-// user.save();
-
-/////////////////////////////////////////
-// user.events.on("change", () => {
-//   console.log("change!");
 // });
 
-// user.events.trigger("change");
+// // user.trigger("change");
+// // user.set({ name: "New nane" });
+// user.fetch();
 
-/////////////////////////////////////////
-// console.log(user.get("name"));
+import { Collection } from "./models/Collection";
 
-user.on("change", () => {
-  console.log(user);
+const collection = new Collection("http://localhost:3000/users");
+collection.on("change", () => {
+  console.log(collection);
 });
+collection.fetch();
 
-// user.trigger("change");
-// user.set({ name: "New nane" });
-user.fetch();
+
