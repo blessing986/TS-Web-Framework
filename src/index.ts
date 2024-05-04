3ea0c869871599010b683317d1a1253fdcd4874c
Blessing U <blessing986@users.noreply.github.com>
2024-05-04 17:14:45 +0100

    chore: generic user collection.

--- src/index.ts
+++ src/index.ts
@@ -69,12 +69,18 @@
 // // user.set({ name: "New nane" });
 // user.fetch();
 
-import { Collection } from "./models/Collection";
+import { User, UserProps } from './models/User';
+import { Collection } from './models/Collection';
 
-const collection = new Collection("http://localhost:3000/users");
-collection.on("change", () => {
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+);
+
+collection.on('change', () => {
   console.log(collection);
 });
+
 collection.fetch();
 
 
