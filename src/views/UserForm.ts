6eba77eb78f3ba8530736f84eac950c96cbebe87
Blessing U <blessing986@users.noreply.github.com>
2024-05-08 07:09:14 +0100

    chore: defining an Events map.

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,17 +1,28 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:button": this.onButtonClick,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log("Hi there");
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input />
+        <button> Click Me </button>
       </div>
     `;
   }
 
   render(): void {
-    const templateElement = document.createElement('template');
+    const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
     this.parent.append(templateElement.content);
